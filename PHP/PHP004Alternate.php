cda43951e731fb9abccc94a1305836086cb65b9b
ultimecreation <34138024+ultimecreation@users.noreply.github.com>
2020-12-16 09:05:47 +0100

    Update PHP004Alternate.php

--- PHP/PHP004Alternate.php
+++ PHP/PHP004Alternate.php
@@ -15,18 +15,14 @@ isAlternate([ 2, 4, 2, 8, 7, 3, 4, 8 ]) -> false
 
 $isAlternate = function($array){
     for($i=0 ; $i<count($array); $i++){
-        if($i == 0){
-
-        }
-        echo "<pre>".print_r($test,true)."</pre>";
-
         if($i == 0) continue;
-        if( ($array[$i] % 2 == 0 && $array[$i-1] % 2 == 1) || ($array[$i] % 2 == 1 && $array[$i-1] % 2 == 0)) return 'true';
-        return 'false';
+        return ($array[$i] % 2 == 0 && $array[$i-1] % 2 == 1) || ($array[$i] % 2 == 1 && $array[$i-1] % 2 == 0) 
+            ? 'true' 
+            : 'false';
+
         // echo "<pre>".print_r($test,true)."</pre>";
     }
 
-};
 
 $isAlternate([ 1, 4, 3, 8, 1, 2, 3, 6 ]);
 echo $isAlternate([ 1, 4, 3, 8, 1, 2, 3, 6 ]).'<br>';
